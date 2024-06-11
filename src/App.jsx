d48747a07a7edfe97c2bc8b1021973f3c60b4f89
mike <josemimore0@gmail.com>
2024-06-11 09:39:13 -0400

    create selection menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,53 @@
+import Students from "./Students.jsx";
+import Supervisors from "./Supervisors.jsx";
 import { useState } from "react";
-import "./App.css";
 import data from "./data.json";
 
 function App() {
-    const [newStudentNameInput, setNewStudentInput] = useState("");
+    const [menuSelection, setMenuSelection] = useState("students");
     const [students, setStudents] = useState(data);
 
-    const addStudent = () => {
-        const newStudentsList = [];
-        newStudentsList.push(...students);
-        const newStudent = {};
-        newStudent.name = newStudentNameInput;
-        newStudentsList.unshift(newStudent);
-        console.log(newStudentsList);
-        setStudents(newStudentsList);
-        setNewStudentInput("");
-    };
-
-    const deleteStudent = (student) => {
-        let newStudentsList = [];
-        newStudentsList.push(...students);
-        console.log(student);
-        newStudentsList = newStudentsList.filter((s) => s.name !== student);
-        setStudents(newStudentsList);
-    };
-
-    return (
-        <div>
-            <div style={{ display: "flex", gap: "20px" }}>
-                <label htmlFor="newStudent">Add new student</label>
-                <input
-                    onChange={(e) => {
-                        setNewStudentInput(e.target.value);
-                    }}
-                    type="text"
-                    name="newStudent"
-                    id="newStudent"
-                    value={newStudentNameInput}
-                />
+    if (menuSelection == "students") {
+        return (
+            <>
                 <button
-                    type="button"
-                    style={{ backgroundColor: "white", color: "black" }}
-                    onClick={addStudent}
+                    onClick={() => {
+                        setMenuSelection("students");
+                    }}
                 >
-                    Add
+                    Students
                 </button>
-            </div>
-            <ul>
-                {students.map((item) => (
-                    <>
-                        <li
-                            key={item.id}
-                            style={{ display: "flex", gap: "10px" }}
-                        >
-                            {item.name}
-                            <button
-                                onClick={() => {
-                                    deleteStudent(item.name);
-                                }}
-                            >
-                                delete
-                            </button>
-                        </li>
-                    </>
-                ))}
-            </ul>
-        </div>
-    );
+                <button
+                    onClick={() => {
+                        setMenuSelection("supervisors");
+                    }}
+                >
+                    Supervisors
+                </button>
+                <Students students={students} setStudents={setStudents} />
+            </>
+        );
+    } else {
+        return (
+            <>
+                <button
+                    onClick={() => {
+                        setMenuSelection("students");
+                    }}
+                >
+                    Students
+                </button>
+                <button
+                    onClick={() => {
+                        setMenuSelection("supervisors");
+                    }}
+                >
+                    Supervisors
+                </button>
+                <Supervisors students={students} setStudents={setStudents} />
+            </>
+        );
+    }
 }
 
 export default App;
---
+++ src/Supervisors.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+
+function Supervisors({ students, setStudents }) {
+    const [newStudentNameInput, setNewStudentInput] = useState("");
+
+    const addStudent = () => {
+        const newStudentsList = [];
+        newStudentsList.push(...students);
+        const newStudent = {};
+        newStudent.name = newStudentNameInput;
+        newStudent.role = "supervisor";
+        newStudentsList.unshift(newStudent);
+        console.log(newStudentsList);
+        setStudents(newStudentsList);
+        setNewStudentInput("");
+    };
+
+    const deleteStudent = (student) => {
+        let newStudentsList = [];
+        newStudentsList.push(...students);
+        console.log(student);
+        newStudentsList = newStudentsList.filter((s) => s.name !== student);
+        setStudents(newStudentsList);
+    };
+
+    const editStudent = (studentName) => {
+        let newStudentsList = [];
+        newStudentsList.push(...students);
+        newStudentsList.map((student) => {
+            if (student.name == studentName) {
+                student.edit ? (student.edit = false) : (student.edit = true);
+            }
+        });
+        setStudents(newStudentsList);
+    };
+
+    const updateName = (e) => {
+        let index = e.target.id;
+        let newStudentsList = [];
+        newStudentsList.push(...students);
+        newStudentsList[index].name = e.target.value;
+        setStudents(newStudentsList);
+    };
+
+    return (
+        <div>
+            <div style={{ display: "flex", gap: "20px" }}>
+                <label htmlFor="newStudent">Add new student</label>
+                <input
+                    onChange={(e) => {
+                        setNewStudentInput(e.target.value);
+                    }}
+                    type="text"
+                    name="newStudent"
+                    id="newStudent"
+                    value={newStudentNameInput}
+                />
+                <button
+                    type="button"
+                    style={{ backgroundColor: "white", color: "black" }}
+                    onClick={addStudent}
+                >
+                    Add
+                </button>
+            </div>
+            <ul>
+                {students.map((item, i) => (
+                    <>
+                        {item.role == "supervisor" && (
+                            <li
+                                key={item.id}
+                                style={{ display: "flex", gap: "10px" }}
+                            >
+                                {item.edit ? (
+                                    <input
+                                        id={i}
+                                        type="text"
+                                        value={item.name}
+                                        onChange={updateName}
+                                    />
+                                ) : (
+                                    item.name
+                                )}
+                                <button
+                                    onClick={() => {
+                                        deleteStudent(item.name);
+                                    }}
+                                >
+                                    delete
+                                </button>
+                                <button
+                                    onClick={() => {
+                                        editStudent(item.name);
+                                    }}
+                                >
+                                    Edit
+                                </button>
+                            </li>
+                        )}
+                    </>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Supervisors;
