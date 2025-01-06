8c219f803212a9f6db38209d0e081834c901dc82
mike <josemimore0@gmail.com>
2025-01-06 16:35:36 -0400

    improve shuffle algorithm

--- src/shuffleStudents/Shuffle.jsx
+++ src/shuffleStudents/Shuffle.jsx
@@ -20,7 +20,9 @@ function Shuffle({ students, tables, setTables }) {
 
     const shuffle = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
+            const arrayBuffer = new Uint32Array(1);
+            window.crypto.getRandomValues(arrayBuffer);
+            const j = arrayBuffer[0] % (i + 1);
             [array[i], array[j]] = [array[j], array[i]];
         }
         return array;
